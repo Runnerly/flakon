8d8ab36abee8345af3ddbc7ae4e62ada4f64fd93
Tarek Ziade <tarek@ziade.org>
2017-07-25 12:07:44 +0200

    added crossdomain snippet

--- flakon/util.py
+++ flakon/util.py
@@ -2,10 +2,12 @@ import os
 import mimetypes
 import json
 import urllib.request
+from functools import update_wrapper
+from datetime import timedelta
 
 import yaml
 from werkzeug.exceptions import HTTPException
-from flask import jsonify, abort
+from flask import jsonify, abort, current_app, request, make_response
 
 
 _JSON_TYPES = ('application/vnd.api+json', 'application/json')
@@ -48,3 +50,46 @@ def error_handling(error):
     resp = jsonify(result)
     resp.status_code = result['code']
     return resp
+
+
+# from http://flask.pocoo.org/snippets/56/
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
