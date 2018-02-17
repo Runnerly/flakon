f24d300f9d66517e31e9a36128b7cc20d401bebb
Tarek Ziade <tarek@ziade.org>
2018-02-17 10:15:50 +0100

    added README

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ with open('requirements.txt') as f:
     deps = [dep for dep in f.read().split('\n') if dep.strip() != '']
     install_requires = deps
 
+with open('README.rst') as f:
+    DESC = f.read()
+
 
 setup(name='flakon',
       version="0.2",
+      description="A collection of opinionated Flask Blueprints",
+      long_description=DESC,
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
